365da27ebcc6aec4ae60b7f32b95e021a0a6e558
coderfocus <songwenjie830@gmail.com>
2019-08-01 15:57:18 +0800

    feat(thymeleaf): 新增th:each

--- spring-boot-thymeleaf-demo/src/main/java/com/coderfocus/springbootthymeleafdemo/BookController.java
+++ spring-boot-thymeleaf-demo/src/main/java/com/coderfocus/springbootthymeleafdemo/BookController.java
@@ -5,7 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -18,6 +20,13 @@ public class BookController {
         model.addAttribute("message","hello world");
         String[] books = {"coder","focus"};
         model.addAttribute("books",books);
+
+        List<User> users = new ArrayList<>();
+        users.add(new User("coder","coder@gmail.com"));
+        users.add(new User("focus","focus@gmail.com"));
+        users.add(new User("array","array@gmail.com"));
+        model.addAttribute("users",users);
+
         return "book";
     }
 }
