59456b233d1cf9dec2c3b76d869466762f7073d0
coderfocus <songwenjie830@gmail.com>
2019-08-09 16:52:49 +0800

    feat(thymeleaf): 解决上传文件大于10M出现连接重置

--- spring-boot-thymeleaf-demo/src/main/java/com/coderfocus/springbootthymeleafdemo/SpringBootThymeleafDemoApplication.java
+++ spring-boot-thymeleaf-demo/src/main/java/com/coderfocus/springbootthymeleafdemo/SpringBootThymeleafDemoApplication.java
@@ -1,7 +1,11 @@
 package com.coderfocus.springbootthymeleafdemo;
 
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootThymeleafDemoApplication {
@@ -10,4 +14,22 @@ public class SpringBootThymeleafDemoApplication {
         SpringApplication.run(SpringBootThymeleafDemoApplication.class, args);
     }
 
+    /** 
+    * @Description: 解决上传文件大于10M出现连接重置的问题。 
+    * @param: []
+    * @return: org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory 
+    * @Author: songwenjie
+    * @Date: 2019/8/9 
+    */ 
+    @Bean
+    public TomcatServletWebServerFactory tomcatEmbedded() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+            if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+                //-1 means unlimited
+                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+            }
+        });
+        return tomcat;
+    }
 }
