3383b7bb4455b2375bd824d8391252b0d93e6376
coderfocus <songwenjie830@gmail.com>
2019-08-12 13:44:43 +0800

    feat(resources): 新增自定义静态资源映射目录

---
+++ spring-boot-thymeleaf-demo/src/main/java/com/coderfocus/springbootthymeleafdemo/configuration/StaticConfiguration.java
@@ -0,0 +1,32 @@
+package com.coderfocus.springbootthymeleafdemo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticConfiguration implements WebMvcConfigurer {
+    /** 
+    * @Description:
+     * Spring Boot 默认为我们提供了静态资源处理，使用 WebMvcAutoConfiguration 中的配置各种属性
+     * 如果我们想自定义静态资源映射目录的话，只需重写addResourceHandlers方法即可
+     * 通过addResourceHandler添加映射路径，然后通过addResourceLocations来指定路径
+     *  会覆盖Spring Boot默认的静态资源处理（WebMvcAutoConfiguration）
+    * @param: [registry]
+    * @return: void 
+    * @Author: songwenjie
+    * @Date: 2019/8/12 
+    */ 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("file:E:/images/") //指定外部路径
+                .addResourceLocations("classpath:/resources/")
+                .addResourceLocations("classpath:/public/")
+                .addResourceLocations("classpath:/static/")
+                .addResourceLocations("classpath:/META-INF/resources/");//指定类路径
+    }
+}
