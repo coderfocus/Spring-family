425c2c80c831109d283a3c896c3a15d9778a7380
coderfocus <songwenjie830@gmail.com>
2019-08-14 17:38:24 +0800

    feat(resources): 新增Spring HttpSession

---
+++ spring-boot-thymeleaf-demo/src/main/java/com/coderfocus/springbootthymeleafdemo/SessionController.java
@@ -0,0 +1,34 @@
+package com.coderfocus.springbootthymeleafdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@RestController
+public class SessionController {
+
+    @GetMapping("/test/session")
+    public Object session(@RequestParam("browser") String browser, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        //取出session中的browser
+        Object sessionBrowser = session.getAttribute("browser");
+        if (sessionBrowser == null) {
+            System.out.println("不存在session，设置browser=" + browser);
+            session.setAttribute("browser", browser);
+        } else {
+            System.out.println("存在session，browser=" + sessionBrowser.toString());
+        }
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie cookie : cookies) {
+                System.out.println(cookie.getName() + " : " + cookie.getValue());
+            }
+        }
+        return sessionBrowser;
+    }
+}
