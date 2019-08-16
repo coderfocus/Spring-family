e969e0d87d81971b027552ba538069f4ec55fcce
coderfocus <songwenjie830@gmail.com>
2019-08-16 15:56:01 +0800

    feat(Servlet): 新增Servlet初始化参数

--- servlet-in-springboot-demo/src/main/java/com/coderfocus/servletinspringbootdemo/servlet/MyServlet.java
+++ servlet-in-springboot-demo/src/main/java/com/coderfocus/servletinspringbootdemo/servlet/MyServlet.java
@@ -1,6 +1,8 @@
 package com.coderfocus.servletinspringbootdemo.servlet;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,14 +10,26 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Writer;
 
-@WebServlet(name="myServlet",urlPatterns = "/myServlet")
+@WebServlet(
+        name="myServlet",
+        urlPatterns = "/myServlet",
+        initParams = {
+               @WebInitParam(name="myname",value = "myvalue")
+        } )
 public class MyServlet extends HttpServlet {
+
+    private String value;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException{
+        this.value = config.getInitParameter("myname");
+    }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         Writer writer = response.getWriter();
-        writer.write("<html><body><h3>hello, my servlet</h3></body></html>");
+        writer.write("<html><body><h3>hello "+ value +", my servlet</h3></body></html>");
     }
 }
