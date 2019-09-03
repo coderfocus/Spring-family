47f4f0d7fa6ce3bc1d9270fa34b9d67bfd8557f0
coderfocus <songwenjie830@gmail.com>
2019-09-03 19:56:49 +0800

    feat(spring-web): 新增Spring自动装载ApplicationContext

---
+++ spring-web/src/main/java/com/coderfocus/controller/AutoUserController.java
@@ -0,0 +1,26 @@
+package com.coderfocus.controller;
+
+import com.coderfocus.commons.context.AutoSpringContext;
+import com.coderfocus.entity.User;
+import com.coderfocus.service.UserService;
+import org.springframework.context.ApplicationContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class AutoUserController extends HttpServlet {
+    ApplicationContext context =  null;
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserService userService = AutoSpringContext.getBean("userService");
+
+        User user = userService.getUser();
+        PrintWriter writer =resp.getWriter();
+        writer.write("name:"+ user.getName() +", email: " +user.getEmail());
+    }
+
+}
