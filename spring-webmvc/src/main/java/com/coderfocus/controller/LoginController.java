4fc368f0d91eae0eccf34358ea793ae76ef89fe8
coderfocus <songwenjie830@gmail.com>
2019-09-10 20:45:59 +0800

    feat(spring-webmvc): 新增Spring Mvc @ResponseBody将结果直接写入HTTP响应正文

--- spring-webmvc/src/main/java/com/coderfocus/controller/LoginController.java
+++ spring-webmvc/src/main/java/com/coderfocus/controller/LoginController.java
@@ -1,10 +1,13 @@
 package com.coderfocus.controller;
 
+import com.coderfocus.dto.User;
 import com.coderfocus.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
@@ -16,5 +19,14 @@ public class LoginController {
     public String login(){
         loginService.login();
         return "login";
+    }
+
+    @RequestMapping(value = "/user",method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<User> getUser(){
+        User user = new User();
+        user.setName("coderfocus");
+        user.setAge(18);
+        return ResponseEntity.ok(user);
     }
 }
